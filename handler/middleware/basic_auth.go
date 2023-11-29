b26379129a2ebb7da618a965a35f6b85ae054d6b
ichiro-ss <sei.swmr@gmail.com>
2023-11-29 12:31:26 +0900

    changed basic_auth.go

--- handler/middleware/basic_auth.go
+++ handler/middleware/basic_auth.go
@@ -9,7 +9,7 @@ import (
 
 func BasicAuth(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-		os.Setenv("BASIC_AUTH_USER_ID", "aaa")
+		os.Setenv("BASIC_AUTH_USER_ID", "admin")
 		os.Setenv("BASIC_AUTH_PASSWORD", "pass")
 
 		basicAuthUserID := os.Getenv("BASIC_AUTH_USER_ID")
@@ -18,13 +18,12 @@ func BasicAuth(h http.Handler) http.Handler {
 		userID, password, ok := r.BasicAuth()
 
 		// 単なる文字列比較だと時間によって長さが推測されてしまう
-		if ok && subtle.ConstantTimeCompare([]byte(userID), []byte(basicAuthUserID)) == 1 && subtle.ConstantTimeCompare([]byte(password), []byte(basicAuthPassword)) == 1 {
-			h.ServeHTTP(w, r)
-		} else {
+		if !ok || userID == "" || password == "" || subtle.ConstantTimeCompare([]byte(userID), []byte(basicAuthUserID)) != 1 || subtle.ConstantTimeCompare([]byte(password), []byte(basicAuthPassword)) != 1 {
 			w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
 			err_str := strconv.Itoa(http.StatusUnauthorized) + ":" + "Unauthorized"
 			http.Error(w, err_str, http.StatusUnauthorized)
-
+		} else {
+			h.ServeHTTP(w, r)
 		}
 	}
 	return http.HandlerFunc(fn)
